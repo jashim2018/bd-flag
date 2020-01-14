1ad281c62a12ee6d6495768d3f438d8e24148e3e
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2020-01-14 15:00:21 +0600

    Update bd-flag.py

--- bd-flag.py
+++ bd-flag.py
@@ -18,7 +18,7 @@ print
 print
 print "\033[1m\033[33m\n\tDeveloper : \033[32m \033[33mT\033[32manjim \033[33mA\033[32mbubokor\033[0m"
 time.sleep(1)
-print "\033[1m\033[32m[+]\033[36m We L😍ve Bangladesh"
+
 time.sleep(1)
 print "\033[1m\033[33m\n\tGithub    :  \033[32mhttp:\033[33m//\033[32mwww.github.com\033[33m/\033[32mtanjim750\033[0m"
 time.sleep(1)
