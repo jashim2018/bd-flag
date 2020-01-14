bc81a9a9f1d6c9fd610ab8964aea667b0e5b762b
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2020-01-14 13:41:05 +0600

    Update bd-flag.py

--- bd-flag.py
+++ bd-flag.py
@@ -1,57 +1,5 @@
  
-#!/usr/bin/python
-#
-#
-# 	<=== IP ATTACK ===>
-#
-#  Name     :  IP Attack
-#  Platform	:  Android Termux
-#  Code     :  Python  
-#  Sec.Code	:  8h4i
-#  Coded by	:  Tanjim Abubokor
-#  Site 		:  
-#  Github 	:  https://github.com/tanjim750   
-#
-#
-#
-# 	<=== WARNING ===>
-#
-# Do Not Copy Or Modify Without Permission..!!!
-#
-#	<=== WARNING ===>
-#
-#
-#
-#
 
-import sys
-import time
-import os
-from urllib2 import *
-from platform import system
-print
-print
-print
-os.system("clear")
-os.system("bash logo.sh")
-print
-print
-print
-print "\033[1m\033[32m\t\t \033[1m\033[31m<==\033[33m[\033[32m Author Info \033[33m]\033[31m==>\033[0m"
-   time.sleep(3)
-print
-print
-print
-print "\033[1m\033[33m\n\tDeveloper : \033[32m \033[33mT\033[32manjim \033[33mA\033[32mbubokor\033[0m"
-time.sleep(1)
-print "\033[1m\033[33m\n\tBlog      : \033[32mhttp:\033[33m//\033[32mtanjim\033[33m4\033[32m750.blogspot.com\033[0m"
-time.sleep(1)
-print "\033[1m\033[33m\n\tGithub    :  \033[32mhttp:\033[33m//\033[32mwww.github.com\033[33m/\033[32mtanjim750\033[0m"
-time.sleep(1)
-print "\033[1m\033[33m\n\tYoutube   : \033[32m \033[33mBB \033[32mAll \033[33mT\033[32mips\033[0m"
-time.sleep(2)
-print
-print
 
 
 
