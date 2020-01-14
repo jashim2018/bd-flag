ec1ee56fd37b4e3087b508d317ae4acb6b043922
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2020-01-14 14:29:13 +0600

    Update bd-flag.py

--- bd-flag.py
+++ bd-flag.py
@@ -16,13 +16,31 @@ time.sleep(3)
 print
 print
 print
-print "\033[1m\033[33m\n\tDeveloper : \033[32m \033[33mS\033[32mutariya \033[33mP\033[32marixit\033[0m"
+print "\033[1m\033[33m\n\tDeveloper : \033[32m \033[33mT\033[32manjim \033[33mA\033[32mbubokor\033[0m"
 time.sleep(1)
-print "\033[1m\033[33m\n\tBlog      :  \033[32mhttp:\033[33m//\033[32mbhai\033[33m4\033[32myou.blogspot.com\033[0m"
+
+print "\033[1m\033[33m\n\tGithub    :  \033[32mhttp:\033[33m//\033[32mwww.github.com\033[33m/\033[32mtanjim750\033[0m"
 time.sleep(1)
-print "\033[1m\033[33m\n\tGithub    :  \033[32mhttp:\033[33m//\033[32mwww.github.com\033[33m/\033[32mBhai4You\033[0m"
-time.sleep(1)
-print "\033[1m\033[33m\n\tYoutube   : \033[32m \033[33mB\033[32mull \033[33mA\033[32mnonymous\033[0m"
+print "\033[1m\033[33m\n\tYoutube   : \033[32m \033[33mBB\033[32mAll \033[33mT\033[32mTips\033[0m"
 time.sleep(2)
 print
 print
+
+
+	print
+	print
+	time.sleep(3)
+	print
+os.system("clear")
+print
+os.system( "bash logo.sh" )
+print
+def mnu():
+	print "\033[1m\033[32m[+]\033[36m We L😍ve Bangladesh"
+print
+print
+print
+mnu()
+
+
+
