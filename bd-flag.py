c9a6d36d81fbd73f9b10778ab32e66649194243d
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2020-01-14 13:46:21 +0600

    Create bd-flag.py

---
+++ bd-flag.py
@@ -0,0 +1,28 @@
+import sys
+import time
+import os
+from urllib2 import *
+from platform import system
+print
+print
+print
+os.system("clear")
+os.system("bash logo.sh")
+print
+print
+print
+print "\033[1m\033[32m\t\t \033[1m\033[31m<==\033[33m[\033[32m Author Info \033[33m]\033[31m==>\033[0m"
+time.sleep(3)
+print
+print
+print
+print "\033[1m\033[33m\n\tDeveloper : \033[32m \033[33mS\033[32mutariya \033[33mP\033[32marixit\033[0m"
+time.sleep(1)
+print "\033[1m\033[33m\n\tBlog      :  \033[32mhttp:\033[33m//\033[32mbhai\033[33m4\033[32myou.blogspot.com\033[0m"
+time.sleep(1)
+print "\033[1m\033[33m\n\tGithub    :  \033[32mhttp:\033[33m//\033[32mwww.github.com\033[33m/\033[32mBhai4You\033[0m"
+time.sleep(1)
+print "\033[1m\033[33m\n\tYoutube   : \033[32m \033[33mB\033[32mull \033[33mA\033[32mnonymous\033[0m"
+time.sleep(2)
+print
+print
