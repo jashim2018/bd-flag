0197cb1bf61e8a20fb3b8ed11fcc21050642219b
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2020-01-14 16:17:52 +0600

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -26,7 +26,7 @@ clear
 toilet -f pagga "Done..!!" | lolcat
 
 echo
-echo -e "\e[1m\e[32m               We \e[31mL😍ve \e[32mBangladesh"
-echo -e "\e[1m\e[32m Developed by :\e[33m Tanjim Bubokor" 
+echo -e "\e[1m\e[32m                            We \e[31mL😍ve \e[32mBangladesh"
+echo -e "\e[1m\e[32m                          Developed by :\e[33m Tanjim Bubokor" 
 
 	
