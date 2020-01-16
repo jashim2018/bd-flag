a6c2444a0f63e28d039f5e43831ba7335714ac4d
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2020-01-16 15:51:25 +0600

    Update bd-flag.py

--- bd-flag.py
+++ bd-flag.py
@@ -18,22 +18,22 @@ print
 print
 print "\033[1m\033[32m\n\t[•]\033[31m─────────────────────────────────────────── \033[32m[•]\033[0m"
 time.sleep(1)
- print "\033[1m\033[32m\n\t|           \033[36mBANGLADESH DARK TERMUX ASSOCIATE           \033[32m|\033[0m"
- time.sleep(1)	
- print "\033[1m\033[32m\n\t|\033[31m──────────────────────────────────────────────\033[32m|
- time.sleep(1)	
- print "\033[1m\033[32m\n\t|                \033[37mAuther : \033[34mBD Termux Team               \033[32m|\033[0m"
- time.sleep(1)	
- print "\033[1m\033[32m\n\t|               \033[37mFacebook : \033[34mTanjim Abubokor             \033[32m|\033[0m"
- time.sleep(1)	
- print "\033[1m\033[32m\n\t|               C\033[37montact WhatsApp : \033[34m01631596698         \033[32m|\033[0m"
- time.sleep(1)	
- print "\033[1m\033[32m\n\t|            \033[37mContact Gmail : \033[34mbdtermuxteam23@gmail.com  \033[32m|\033[0m"
- time.sleep(1)	
- print "\033[1m\033[32m\n\t[•] \033[31m─────────────────────────────────────────── \033[32m[•]\033[0m"
- time.sleep(2)
-
-
+print "\033[1m\033[32m\n\t|           \033[36mBANGLADESH DARK TERMUX ASSOCIATE           \033[32m|\033[0m"
+time.sleep(1)	
+print "\033[1m\033[32m\n\t|\033[31m──────────────────────────────────────────────\033[32m|
+time.sleep(1)	
+print "\033[1m\033[32m\n\t|                \033[37mAuther : \033[34mBD Termux Team               \033[32m|\033[0m"
+time.sleep(1)	
+print "\033[1m\033[32m\n\t|               \033[37mFacebook : \033[34mTanjim Abubokor             \033[32m|\033[0m"
+time.sleep(1)	
+print "\033[1m\033[32m\n\t|               C\033[37montact WhatsApp : \033[34m01631596698         \033[32m|\033[0m"
+time.sleep(1)	
+print "\033[1m\033[32m\n\t|            \033[37mContact Gmail : \033[34mbdtermuxteam23@gmail.com  \033[32m|\033[0m"
+time.sleep(1)	
+print "\033[1m\033[32m\n\t[•] \033[31m─────────────────────────────────────────── \033[32m[•]\033[0m"
+time.sleep(2)
+print
+print
      						
 
 
