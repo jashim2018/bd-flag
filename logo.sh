0e4a7a6f11fe5977e194e23307407830d682e65c
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2020-01-16 10:01:32 +0600

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -15,15 +15,7 @@ echo -e "\e[1m\e[32m████████████████████
 
 
 toilet -f pagga "Bangladesh" | lolcat
-#usr/bin/bash
-clear
-bi='\033[34;1m' #biru
-i='\033[32;1m' #ijo
-pur='\033[35;1m' #purple
-cy='\033[36;1m' #cyan
-me='\033[31;1m' #merah
-pu='\033[37;1m' #putih
-ku='\033[33;1m' #kuning
+
 echo
 echo
 echo
