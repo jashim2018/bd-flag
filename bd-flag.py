c957355a65af51fed9fd0d4ba2cf36f6a41d3d71
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2020-01-16 12:51:28 +0600

    Update bd-flag.py

--- bd-flag.py
+++ bd-flag.py
@@ -36,7 +36,19 @@ print "\033[1m\033[32m\n\tprint "\033[1m\033[32m[\033\34m•\033[32m]───�
 time.sleep(2)
 print
 print     
-					     
+print
+print
+print
+print "\033[1m\033[33m\n\tDeveloper : \033[32m \033[33mS\033[32mutariya \033[33mP\033[32marixit\033[0m"
+time.sleep(1)
+print "\033[1m\033[33m\n\tBlog      :  \033[32mhttp:\033[33m//\033[32mbhai\033[33m4\033[32myou.blogspot.com\033[0m"
+time.sleep(1)
+print "\033[1m\033[33m\n\tGithub    :  \033[32mhttp:\033[33m//\033[32mwww.github.com\033[33m/\033[32mBhai4You\033[0m"
+time.sleep(1)
+print "\033[1m\033[33m\n\tYoutube   : \033[32m \033[33mB\033[32mull \033[33mA\033[32mnonymous\033[0m"
+time.sleep(2)
+print
+print					     
 
 
 
