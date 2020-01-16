e76e4c6ee2c6d94f7c8e1b767883bf26f7f64503
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2020-01-16 12:14:38 +0600

    Update bd-flag.py

--- bd-flag.py
+++ bd-flag.py
@@ -11,20 +11,25 @@ os.system("bash logo.sh")
 print
 print
 print
-print "\033[1m\033[32m\t\t \033[1m\033[31m<==\033[33m[\033[32m We \033[31mLove \033[32mBangladesh \033[33m]\033[31m==>\033[0m"
+print "\033[1m\033[32m\t\t \033[1m\033[31m<==\033[33m[\033[32m We \033[31mLove \033[32mBangladesh \033[32m]\033[31m==>\033[0m
+"
 time.sleep(3)
 print
 
+print "\033[1m\033[32m[\033\34m•\033\32m]"───────────────────────────────────────────\033[1m\033[32m[\033\34m•\033\32m]"
+print "\033[1m\033[32m\n\t BANGLADESH DARK TERMUX ASSOCIATE         \033[32m||"
+print "\033[1m\033[33m\n\t─────────────────────────────────────────────"$i"\033[32m||"
+print "\033[1m\033[33m\n\tAUTHOR : \003[36m[BD Termux Team]                   \033[32m|"
+print "\033[1m\033[33m\n\tFACEBOOK : \033\36mTanjim Abubokor                   \033[32m||"
+print "\033[1m\033[33m\n\tContack WhatsApp : \033[36m01631596698             \033[32m||"
+print "\033[1m\033[33m\n\tContack Gmail : \033[36mbdtermuxteam23@gmail.com     \033[32m||"
+print "\033[1m\033[33m\n\tprint "\033[1m\033[32m[\033\34m•\033\32m]"───────────────────────────────────────────\033[1m\033[32m[\033\34m•\033\32m]"
+#\033[0m
+					     
 
 
 
 	
-
-
-
-
-
-
 
 
 
