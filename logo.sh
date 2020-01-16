65ef92c411c91ddf52f63e4de72f5ea203166cbc
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2020-01-16 09:58:58 +0600

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -15,17 +15,25 @@ echo -e "\e[1m\e[32m████████████████████
 
 
 toilet -f pagga "Bangladesh" | lolcat
-    
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
 echo
 echo
 echo
 echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
 echo $i" |"$cy"     BANGLADESH DARK TERMUX ASSOCIATE"$i"         |"
-echo $i" |"$me"─────────────────────────────────────────────"$i"|"
-echo $i" |"$pu" AUTHOR :"$ku" [BD Termux Team]"$i"                   |"
+echo $i" |"$me"─────────────────────────────────────────────"$i" |"
+echo $i" |"$pu" AUTHOR :"$ku" [BD Termux Team]"$i"                    |"
 echo $i" |"$pu" FACEBOOK :"$ku" Tanjim Abubokor"$i"                   |"
-echo $i" |"$pu" Contack WhatsApp :"$ku" 01631596698"$i"             |"
-echo $i" |"$pu" Contack Gmail :"$ku" bdtermuxteam23@gmail.com"$i"      |"
+echo $i" |"$pu" Contack WhatsApp :"$ku" 01631596698"$i"               |"
+echo $i" |"$pu" Contack Gmail :"$ku" bdtermuxteam23@gmail.com"$i"     |"
 echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
 echo
 
