154d9d4cf603cb492bf8cd482d060cc8804cb836
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2020-01-16 16:41:11 +0600

    Update bd-flag.py

--- bd-flag.py
+++ bd-flag.py
@@ -16,15 +16,19 @@ time.sleep(3)
 print
 print
 print
-print "\033[1m\033[37m\n\tAuthor : \033[32m \033[37mBD\033[34mTermux \033[37mT\033[37meam\033[0m"
+print "\033[1m\033[32m[+]\033[31m───────────────────────────────────────────\033[32m[+]\033[0m"
 time.sleep(1)
-print "\033[1m\033[37m\n\tFacebook      :  \033[34mTanjim \033[34mAbubokor\033[0m"
+print "\033[1m\033[37m\n\tAuthor  : \033[32m \033[3m4BD \033[34mTermux \033[3m4T\033[34meam\033[0m"
+time.sleep(1)
+print "\033[1m\033[37m\n\tFacebook :  \033[34mTanjim \033[34mAbubokor\033[0m"
 
 time.sleep(1)
 print "\033[1m\033[37m\n\tGithub    :  \033[33mhttp:\033[33m//\033[33mwww.github.com\033[33m/\033[33mtanjim750\033[0m"
 time.sleep(1)
 print "\033[1m\033[37m\n\tContact   : \033[34mbdtermuxteam23@gmail.com\033[0m"
 time.sleep(2)
+print "\033[1m\033[32m[+]\033[31m───────────────────────────────────────────\033[32m[+]\033[0m"
+time.sleep(1)
 print
 print
      						
