94fafa56a2145ef5a11fdce3c32957a6fb288cc4
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2020-01-16 09:41:45 +0600

    Update bd-flag.py

--- bd-flag.py
+++ bd-flag.py
@@ -15,39 +15,7 @@ print "\033[1m\033[32m\t\t \033[1m\033[31m<==\033[33m[\033[32m We \033[31mLove \
 time.sleep(3)
 print
 
-#usr/bin/bash
-clear
-bi='\033[34;1m' #biru
-i='\033[32;1m' #ijo
-pur='\033[35;1m' #purple
-cy='\033[36;1m' #cyan
-me='\033[31;1m' #merah
-pu='\033[37;1m' #putih
-ku='\033[33;1m' #kuning
-# Jangan Recode Ya gayn
-echo
-echo
-echo
-echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
-echo $i" |"$cy"     BANGLADESH DARK TERMUX ASSOCIATE"$i"         |"
-echo $i" |"$me"─────────────────────────────────────────────"$i"|"
-echo $i" |"$pu" AUTHOR :"$ku" [BD Termux Team]"$i"                   |"
-echo $i" |"$pu" FACEBOOK :"$ku" Tanjim Abubokor"$i"                   |"
-echo $i" |"$pu" Contack WhatsApp :"$ku" 01631596698"$i"             |"
-echo $i" |"$pu" Contack Gmail :"$ku" bdtermuxteam23@gmail.com"$i"      |"
-echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
-echo
-echo $i"────────────────────────────────    ─────"
-echo $i"|"$me" 1"$i" |"$cy" Facebook Auto Tools        "$i"|"$i"       |"$cy" B"$i" |"
 
-
-echo $i"|"$me" 2"$i" |"$cy" Facebook Hack [Phishing]   "$i"|"$i"       |"$cy" E"$i" |"
-echo $i"|"$me" 3"$i" |"$cy" Facebook Hack [BruteForce] "$i"|"$i"       |"$cy" N"$i" |"
-echo $i"────────────────────────────────"$i"                      |"$cy" L"$i" |"
-
-
-
-    
 
 
 	
